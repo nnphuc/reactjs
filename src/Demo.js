a46effcd5e92c096eba2b76601d7ec6cc916a8ca
Nguyen Ngoc Phuc <nguyenngocphuc@outlook.com>
2018-05-08 22:17:56 +0700

    03

---
+++ src/Demo.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Reactjs</h1>
+                <p>
+                    chao mung den voi khoa hoc reactjs    
+                </p>
+                
+            </div>
+        );
+    }
+}
+
+export default Demo;
