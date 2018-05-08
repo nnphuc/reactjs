77d439bb4ba23aa74cd8d0249d4c3f1934d6df60
Nguyen Ngoc Phuc <nguyenngocphuc@outlook.com>
2018-05-08 22:32:58 +0700

    lession 02

--- lession02/src/index.js
+++ lession02/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Demo from './Demo';
-
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Demo />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
